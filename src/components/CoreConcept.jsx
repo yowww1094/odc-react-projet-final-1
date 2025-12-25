e1d4fcc892442fd7cd382e3fe139cfb8983e6188
hiba ait abdellah <hibaaitabdellah81@gmail.com>
2025-12-25 19:41:59 +0100

    adding concept cores with 3 methodes

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function CoreConcept({Image,Titre,Description}) {
+function CoreConcept({image,title,description}) {
   return (
     <section>
-       <img src={Image} alt="" />
-      <h1>{Titre}</h1>
-      <p>{Description}</p>
+       <img src={image} alt="" />
+      <h1>{title}</h1>
+      <p>{description}</p>
     </section>
      
   )
