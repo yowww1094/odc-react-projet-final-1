1cc24f23633e5fae9a7249f292c997b47a6b2d56
Younes AIT SI ABBOU <younesaitsiabou@gmail.com>
2025-12-25 14:33:08 +0100

    initial files added

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CoreConcept() {
+  return (
+    <div>CoreConcept</div>
+  )
+}
+
+export default CoreConcept
---
+++ src/components/Header/TabButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TabButton() {
+  return (
+    <div>TabButton</div>
+  )
+}
+
+export default TabButton
