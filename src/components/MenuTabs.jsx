3b2128904a0e3105b99da6944bb435e997352597
Aj-ziad <ajdourziyad@gmail.com>
2025-12-26 09:19:53 +0100

    fix the text

--- src/components/MenuTabs.jsx
+++ src/components/MenuTabs.jsx
@@ -3,34 +3,37 @@ import TabButton from "./TabButton";
 import { EXAMPLES } from "../data.js";
 
 export default function MenuTabs() {
-  const [selected, setSelected] = useState("components");
+  const [selected, setSelected] = useState(null); 
+
+  const selectedExample = EXAMPLES.find((e) => e.id === selected);
 
   return (
     <>
-     <h1> Examples</h1>
-    <div>
-      {EXAMPLES.map(example => (
-        <TabButton
-          key={example.id}
-          isSelected={selected === example.id}
-          onSelect={() => setSelected(example.id)}
-        >
-          {example.title}
-        </TabButton>
-      ))}
+      <h1>Examples</h1>
 
-      
-      <div className="tab-content">
-        <h2>{EXAMPLES.find(e => e.id === selected).title}</h2>
-        <p>{EXAMPLES.find(e => e.id === selected).description}</p>
-        <pre>
-          <code>
-            {EXAMPLES.find(e => e.id === selected).code}
-          </code>
-        </pre>
+      <div>
+        {EXAMPLES.map((example) => (
+          <TabButton
+            key={example.id}
+            isSelected={selected === example.id}
+            onSelect={() => setSelected(example.id)}
+          >
+            {example.title}
+          </TabButton>
+        ))}
       </div>
-    </div>
+
+      {selected
+        ? selectedExample && (
+            <div className="tab-content">
+              <h2>{selectedExample.title}</h2>
+              <p>{selectedExample.description}</p>
+              <pre>
+                <code>{selectedExample.code}</code>
+              </pre>
+            </div>
+          )
+        : <h1>Pleas select a topic </h1>}
     </>
-   
   );
 }
